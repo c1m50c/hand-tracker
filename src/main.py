8b2fb96a550e2f436b47181069c6d9b877b083ec
c1m50c <terminalityms@gmail.com>
2021-11-12 14:19:50 -0600

    Finger Tip Indicator 🖕

--- src/main.py
+++ src/main.py
@@ -3,32 +3,49 @@ import time
 import cv2
 
 
-capture = cv2.VideoCapture(0)
-mp_draw = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
-
-previous_time = 0
-current_time = 0
-
-
 if __name__ == "__main__":
+    FINGER_TIPS = [ 4, 8, 12, 16, 20 ]
+    
+    capture = cv2.VideoCapture(0)
+    mp_draw = mp.solutions.drawing_utils
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
+
+    previous_time = 0
+    current_time = 0
+
     while True:
         success, image = capture.read()
+        image.flags.writeable = False
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = hands.process(image_rgb)
         
         hand_count: int = 0
+        number_count: int = 0
+        image.flags.writeable = True
         if results.multi_hand_landmarks:
             hand_count = len(results.multi_hand_landmarks)
-            # For each hand visible draw the connections and landmarks
             for hand_lm in results.multi_hand_landmarks:
+                # For each hand visible draw finger tips
+                for idx, lm in enumerate(hand_lm.landmark):
+                    height, width, _ = image.shape
+                    x, y = int(lm.x * width), int(lm.y * height)
+                    if idx in FINGER_TIPS:
+                        cv2.circle(
+                            img=image,
+                            center=(x, y),
+                            radius=10,
+                            color=(0, 0, 255),
+                            thickness=2,
+                        )
+                
+                # For each hand visible draw the connections and landmarks
                 mp_draw.draw_landmarks(
                     image=image,
                     landmark_list=hand_lm,
                     connections=mp_hands.HAND_CONNECTIONS,
                     landmark_drawing_spec = mp_draw.DrawingSpec(color=(255, 255, 255)), # Point Spec
-                    connection_drawing_spec = mp_draw.DrawingSpec(color=(0, 0, 255)), # Line Spec
+                    connection_drawing_spec = mp_draw.DrawingSpec(color=(255, 0, 0)), # Line Spec
                 )
         
         current_time = time.time()
@@ -42,7 +59,7 @@ if __name__ == "__main__":
             org=(10, 20),
             fontFace=cv2.FONT_HERSHEY_PLAIN,
             fontScale=1,
-            color=(128, 128, 128),
+            color=(255, 255, 255),
             thickness=1,
         )
         
@@ -53,7 +70,18 @@ if __name__ == "__main__":
             org=(10, 35),
             fontFace=cv2.FONT_HERSHEY_PLAIN,
             fontScale=1,
-            color=(128, 128, 128),
+            color=(255, 255, 255),
+            thickness=1,
+        )
+        
+        # Number Count
+        cv2.putText(
+            img=image,
+            text=f"Number: {number_count}",
+            org=(10, 50),
+            fontFace=cv2.FONT_HERSHEY_PLAIN,
+            fontScale=1,
+            color=(255, 255, 255),
             thickness=1,
         )
         
